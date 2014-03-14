5be642af1869638f68ced10cc7d733c58e1b0cb5
Evan Lloyd <eulloyd@gmail.com>
2014-03-14 16:24:40 +0900

    Fix event listener

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 $(document).ready(function (){
 	$('.twerk i').on('mouseover', function(){
-		$('#shoe-box').toggleClass("shake shake-constant")
-		$('#form').toggleClass("shake shake-constant")
-		$('#app').toggleClass("shake shake-constant")
+		$('#form').toggleClass("shake shake-slow shake-constant")
+		$('#app').toggleClass("shake shake-crazy shake-constant")
 	});
 
 	$('.submit').on("click", function(){
-		
 		var shoe = $(this).closest('#form').find('#shoe').val();
 		var price = $(this).closest('#form').find('#price').val();
 		var store = $(this).closest('#form').find('#store').val();
+		console.log(shoe)
+		console.log(price)
+		console.log(store)
+		var face = $('<div class="shoes">'+shoe+price+store+'</div>')
 		var shoes = $('<div class="shoes">'+
-			'<input type="checkbox" name="buy">'+
+			'<input type="checkbox" name="buy" class="check">'+
 			'<p class="name">'+shoe+'</p>'+
 			'<p class="price"><i class="fa fa-usd"></i>'+price+'</p>'+
 			'<p class="store"><i class="fa fa-home"></i>'+store+'</p>'+
 			'<div class="trash shake shake-horizontal"><i class="fa fa-trash-o"></i></div>'+
 			'</div>');
-		$(".shoe-box").append(shoes);
-	});
+		$("#shoe-box").append(shoes);
+	})
 
-	$('.trash').on("click", function(){
+	$('body').on("click", ".trash", function(){
 		$(this).closest('.shoes').fadeOut()
+		console.log('trash clicked')
 	});
 
-	$('.check').on("click", function(){
+	$('body').on("click", '.check', function(){
 		$(this).closest('.shoes').fadeTo("slow", 0.5)
+		console.log('check')
 	});
 });
